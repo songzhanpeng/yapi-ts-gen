121b30055f7c582b6c9acf7c6e9d409386500b69
songzhanpeng <songzhanpeng@iauto.com>
2025-06-06 11:12:05 +0800

    更新YAPI配置，修改输出目录和文件名，新增自定义输出规则功能
    - 将输出目录更改为"./tcms"，输出文件名更改为"index.ts"。
    - 新增自定义输出规则，支持将多个接口根据路径匹配分组到同一个文件中。
    - 更新yapiUrl以适应新的API接口配置。

--- yapi.config.js
+++ yapi.config.js
@@ -1,14 +1,21 @@
 const config = [
   {
-    "yapiUrl": "http://iauto-yapi.ci.iauto.com/api/open/plugin/export-full?type=json&pid=545&status=all&token=0b8c264fd29c94bea17a897867815943f0e2c89b9a04a0920378ce457eb1906d",
-    "outputDir": "./src/api",
-    "outputFileName": "api.ts",
-    "namespace": "API",
-    "stripPathPrefixes": [
-      "/svc/api/v1",
-    ],
-    "requestImportPath": "@/utils/request",
-    "yapiBaseUrl": "http://iauto-yapi.ci.iauto.com"
+    yapiUrl: "http://iauto-yapi.ci.iauto.com/api/open/plugin/export-full?type=json&pid=548&status=all&token=xxxxx",
+    outputDir: "./tcms",
+    outputFileName: "index.ts", // 默认输出文件，不匹配自定义规则的接口会输出到这里
+    namespace: "TCMS",
+    stripPathPrefixes: ["/api/trace_matrix"],
+    requestImportPath: "@/hooks/web/useAxios",
+
+    // 新功能：自定义输出规则 - 多个接口输出到同一个文件npm
+    customOutputs: [
+      {
+        matches: [ // 多个路径前缀，以这些路径开头的接口会被分组到同一个文件
+          "/api",
+        ],
+        outputFileName: "feature.ts", // 输出文件名
+      },
+    ]
   }
 ];
 
